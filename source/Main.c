8f5e7a29175dfb448d4ed09e463e3ff16bcfe8bc
s1206219-bit <s1206219@taivs.tp.edu.tw>
2026-09-20 15:18:44 +0800

    first commit

---
+++ source/Main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	printf("*********\n");
+	printf("*");
+	printf("       *\n");
+	printf("*");
+	printf("       *\n");
+	printf("*");
+	printf("       *\n");
+	printf("*");
+	printf("       *\n");
+	printf("*");
+	printf("       *\n");
+	printf("*");
+	printf("       *\n");
+	printf("*");
+	printf("       *\n");
+	printf("*********\n");
+}
